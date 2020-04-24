f385684114276f42d0232e47e61f0971dea4beb0
Archit Gupta <archit.gupta@booking.com>
2020-04-25 01:43:17 +0200

    Leetcode Create 1

---
+++ src/main/java/com/archit/coding/leetcode/TwoSum.java
@@ -0,0 +1,23 @@
+package com.archit.coding.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+  class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      int [] result = new int[2];
+      Map<Integer, Integer> hash = new HashMap();
+      for(int i = 0; i< nums.length; i++) {
+        int complement = target - nums[i];
+        if(hash.containsKey(complement)) {
+          result[0] = hash.get(complement);
+          result[1] = i;
+          return result;
+        }
+        hash.put(nums[i], i);
+      }
+      return result;
+    }
+  }
+}
